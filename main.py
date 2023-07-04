8d41c1dc43b7073e331593e6dc131a999da6f500
magical <sun920122@gmail.com>
2023-07-04 16:15:07 +0800

    web gui init test

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
-from PDFEditor import PDFMaker
+from flask import Flask
+
+from app import create_app
 
 # test
-pdf = PDFMaker()
-pdf.eventNameInput('活動名稱')
-pdf.groupNameInput('社團名稱')
-pdf.singleInput('活動目的')
-pdf.multipleInput('活動目的', 10000, 1000, '次', 1000000)
-pdf.multipleInput('活動項目', 100000, 10000, '次', 10000000)
-pdf.multipleInput('活動目的', 1000000, 100000, '次', 100000000, comma=True)
-pdf.updateFilename('test')
-pdf.save()
+# pdf = PDFMaker()
+# pdf.eventNameInput('活動名稱')
+# pdf.groupNameInput('社團名稱')
+# pdf.singleInput('活動目的')
+# pdf.multipleInput('活動目的', 10000, 1000, '次', 1000000)
+# pdf.multipleInput('活動項目', 100000, 10000, '次', 10000000)
+# pdf.multipleInput('活動目的', 1000000, 100000, '次', 100000000)
+# pdf.updateFilename('test')
+# pdf.save()
+
+DEBUG = True
+
+app = create_app()
+
+if __name__ == '__main__':
+    app.run(debug=DEBUG, port=12345)
