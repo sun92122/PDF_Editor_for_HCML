2a22fe38a6eec689edd70a64f0b259abf701920c
magical <sun920122@gmail.com>
2023-08-03 15:34:44 +0800

    coreui

---
+++ frontend/src/layouts/DefaultLayout.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <Sidebar />
+    <div class="wrapper d-flex flex-column min-vh-100 bg-light">
+      <Header></Header>
+      <div class="body flex-grow-1 px-3">
+        <CContainer>
+          <router-view />
+        </CContainer>
+      </div>
+      <Footer></Footer>
+    </div>
+  </div>
+</template>
+<script>
+import { CContainer } from "@coreui/vue";
+import Sidebar from "@/components/Sidebar.vue";
+import Header from "@/components/Header.vue";
+import Footer from "@/components/Footer.vue";
+
+export default {
+  name: "DefaultLayout",
+  components: {
+    Sidebar,
+    Header,
+    Footer,
+    CContainer,
+  },
+};
+</script>
